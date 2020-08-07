8673a61dc716369dd0ade5172c947d2b9f89ba74
Hari Hara Kumar Nakshatrala <harinakshatrala@MacBook-Pro.local>
2020-08-07 15:45:52 -0700

    Program to check if there is a palindrome in all the permutations

---
+++ palindrome_in_permutations.py
@@ -0,0 +1,51 @@
+import unittest
+
+
+def has_palindrome_permutation(the_string):
+    if len(the_string) < 2:
+        return True
+
+    count_set = set()
+    for char in the_string:
+        if char in count_set:
+            count_set.remove(char)
+        else:
+            count_set.add(char)
+
+    if len(count_set) < 2:
+        return True
+    else:
+        return False
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcbcd')
+        self.assertTrue(result)
+
+    def test_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabccbdd')
+        self.assertTrue(result)
+
+    def test_no_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcd')
+        self.assertFalse(result)
+
+    def test_no_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabbcd')
+        self.assertFalse(result)
+
+    def test_empty_string(self):
+        result = has_palindrome_permutation('')
+        self.assertTrue(result)
+
+    def test_one_character_string(self):
+        result = has_palindrome_permutation('a')
+        self.assertTrue(result)
+
+
+unittest.main(verbosity=2)
+
