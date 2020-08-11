63862d4f855bed1d06d128aaae6cdeb1e4da64fc
Hari Hara Kumar Nakshatrala <harinakshatrala@MacBook-Pro.local>
2020-08-10 17:00:17 -0700

    Apple stock prices profit greedy solution in O(n)

---
+++ profilt_from_apple_stock_prices.py
@@ -0,0 +1,55 @@
+def get_max_profit(stock_prices):
+    cur_max = stock_prices[1] - stock_prices[0]
+    cur_min = stock_prices[0]
+    for i in range(1, len(stock_prices)):
+        if stock_prices[i] - cur_min > cur_max:
+            cur_max = stock_prices[i] - cur_min
+        if stock_prices[i] < cur_min:
+            cur_min = stock_prices[i]
+    return cur_max
+
+
+
+# Tests
+
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def test_price_goes_up_then_down(self):
+        actual = get_max_profit([1, 5, 3, 2])
+        expected = 4
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_down_then_up(self):
+        actual = get_max_profit([7, 2, 8, 9])
+        expected = 7
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_up_all_day(self):
+        actual = get_max_profit([1, 6, 7, 9])
+        expected = 8
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_down_all_day(self):
+        actual = get_max_profit([9, 7, 4, 1])
+        expected = -2
+        self.assertEqual(actual, expected)
+
+    def test_price_stays_the_same_all_day(self):
+        actual = get_max_profit([1, 1, 1, 1])
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_error_with_empty_prices(self):
+        with self.assertRaises(Exception):
+            get_max_profit([])
+
+    def test_error_with_one_price(self):
+        with self.assertRaises(Exception):
+            get_max_profit([1])
+
+
+unittest.main(verbosity=2)
+
